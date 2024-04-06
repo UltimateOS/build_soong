43cf1f04c5c018599174bbfe76ba66f7393e1f97
John Galt <johngaltfirstrun@gmail.com>
2024-04-06 01:33:32 -0400

    lto: enable O3 optimizations on explicit lto modules
    Change-Id: Ia635a98b1c241a3b90029148fcd761c4dffb8d6b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/lto.go
+++ cc/lto.go
@@ -110,6 +110,7 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 	}
 	if lto.Properties.LtoEnabled {
 		ltoCFlags := []string{"-flto=thin", "-fsplit-lto-unit"}
+		var ltoCOnlyFlags []string
 		var ltoLdFlags []string
 
 		// The module did not explicitly turn on LTO. Only leverage LTO's
@@ -119,6 +120,9 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 		// Apply the same for Eng builds as well.
 		if !lto.ThinLTO() || ctx.Config().Eng() {
 			ltoLdFlags = append(ltoLdFlags, "-Wl,--lto-O0")
+		} else {
+			ltoLdFlags = append(ltoLdFlags,"-Wl,--lto-O3")
+			ltoCOnlyFlags = append(ltoCOnlyFlags, "-O3")
 		}
 
 		if Bool(lto.Properties.Whole_program_vtables) {
@@ -164,6 +168,7 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 		flags.Local.AsFlags = append(flags.Local.AsFlags, ltoCFlags...)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoCFlags...)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoLdFlags...)
+		flags.Local.CFlags = append(flags.Local.CFlags, ltoCOnlyFlags...)
 	}
 	return flags
 }
